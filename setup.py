644149b94eb03c1bc9d1f766c0e14117291d2c20
tbshiki <info@tbshiki.com>
2023-03-01 10:42:05 +0900

    fix Corrections and information added

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 from setuptools import setup, find_packages
 
 NAME = "seleniumhelper"
-AUTHOR = "tbshiki"
-AUTHOR_EMAIL = "info@tbshiki.com"
-URL = "https://github.com/tbshiki/seleniumhelper"
 VERSION = "0.1.1"
 PYTHON_REQUIRES = ">=3.9"
 INSTALL_REQUIRES = [
@@ -13,5 +10,9 @@ INSTALL_REQUIRES = [
     "os",
 ]
 
+AUTHOR = "tbshiki"
+AUTHOR_EMAIL = "info@tbshiki.com"
+URL = "https://github.com/tbshiki/" + NAME
 
-setup(name=NAME, author=AUTHOR, author_email=AUTHOR_EMAIL, version=VERSION, python_requires=PYTHON_REQUIRES, install_requires=INSTALL_REQUIRES, packages=find_packages())
+
+setup(name=NAME, author=AUTHOR, author_email=AUTHOR_EMAIL, url=URL, version=VERSION, python_requires=PYTHON_REQUIRES, install_requires=INSTALL_REQUIRES, packages=find_packages())
